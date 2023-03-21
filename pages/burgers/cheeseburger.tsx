2a35797c6a5fdcd76ce9892bdf29debb28a1fbe0
Diana Murtazina <murka8831@yandex.ru>
2023-03-21 23:55:19 +0400

    creating new pages

---
+++ pages/burgers/cheeseburger.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Cheeseburger = (props: Props) => {
+    return (
+        <div>
+            <h1>Чизбургер</h1>
+        </div>
+    );
+};
+
+export default Cheeseburger;
---
+++ pages/burgers/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Burgers = (props: Props) => {
+    return (
+        <div>
+            <h1>Наши бургеры</h1>
+        </div>
+    );
+};
+
+export default Burgers;
