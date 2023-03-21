efbcf49ec7ed2a11f4410519a4234663af2f5d3c
Diana Murtazina <murka8831@yandex.ru>
2023-03-22 00:08:08 +0400

    added components

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+    return <footer>Copyright 2023 - Fat Burgers</footer>;
+};
+
+export default Footer;
--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { SiBurgerking } from "react-icons/si";
 
 type Props = {};
 
 const Header = (props: Props) => {
-    return <div>Header</div>;
+    return (
+        <header>
+            <div>
+                <SiBurgerking />
+            </div>
+            <nav>
+                <a>Домой</a>
+                <a>О нас</a>
+                <a>Отзывы</a>
+                <a>Бургеры</a>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
