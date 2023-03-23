5125b5804cf76ef6fd4e35432131aa41750b24d8
Diana Murtazina <murka8831@yandex.ru>
2023-03-23 23:25:13 +0400

    added route to main page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,23 @@
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
 type Props = {};
 
 const NotFoundPage = (props: Props) => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/");
+        }, 5000);
+    }, []);
     return (
         <div className="not-found">
             <h1>Ой...</h1>
             <h2>Такой страницы здесь нет!</h2>
             <p>
-                Перейти на <Link href="/">главную страницу</Link>{" "}
+                Переход на <Link href="/">главную страницу</Link> через 5 секунд{" "}
             </p>
         </div>
     );
