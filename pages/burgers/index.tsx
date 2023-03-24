1c5c7d2495de0364b2ac1f99f5dfa0a16774ffb9
Diana Murtazina <murka8831@yandex.ru>
2023-03-24 12:48:54 +0400

    creating a json server, getting data from the getStaticProps function

--- pages/burgers/index.tsx
+++ pages/burgers/index.tsx
@@ -1,9 +1,25 @@
 import React, { FC } from "react";
 import styles from "../../styles/Burgers.module.css";
 
-type Props = {};
+type Props = {
+    name: string;
+    image: string;
+    desc: string;
+    price: number;
+    id: string;
+};
 
-const Burgers: FC = (props: Props) => {
+export const getStaticProps = async () => {
+    const res = await fetch("http://localhost:5000/items");
+    const data = await res.json();
+
+    return {
+        props: { burgers: data },
+    };
+};
+
+const Burgers: FC<Props[]> = (props) => {
+    console.log(props);
     return (
         <div>
             <h1>Наши бургеры</h1>
