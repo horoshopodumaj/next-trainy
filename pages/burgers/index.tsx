a9e1d8472c21a6c22f131265991d689e4ce8531e
Diana Murtazina <murka8831@yandex.ru>
2023-03-24 13:17:37 +0400

    getting all burgers on the page

--- pages/burgers/index.tsx
+++ pages/burgers/index.tsx
@@ -1,13 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
 import React, { FC } from "react";
 import styles from "../../styles/Burgers.module.css";
 
-type Props = {
+type IProps = {
     name: string;
     image: string;
     desc: string;
     price: number;
     id: string;
 };
+
+interface IBurgers {
+    burgers: IProps[];
+}
 
 export const getStaticProps = async () => {
     const res = await fetch("http://localhost:5000/items");
@@ -18,11 +24,33 @@ export const getStaticProps = async () => {
     };
 };
 
-const Burgers: FC<Props[]> = (props) => {
-    console.log(props);
+const Burgers: FC<IBurgers> = ({ burgers }) => {
     return (
         <div>
             <h1>Наши бургеры</h1>
+            {burgers.map((burger) => {
+                return (
+                    <Link
+                        className={styles.burgerCard}
+                        href={`burgers/${burger.id}`}
+                        key={burger.id}>
+                        <div className={styles.imageContainer}>
+                            <Image
+                                src={`${burger.image}`}
+                                alt={`${burger.name}`}
+                                width={200}
+                                height={200}
+                                layout="responsive"
+                                objectFit="cover"
+                            />
+                        </div>
+                        <div>
+                            <h3>{burger.name}</h3>
+                            <p>{burger.desc}</p>
+                        </div>
+                    </Link>
+                );
+            })}
         </div>
     );
 };
